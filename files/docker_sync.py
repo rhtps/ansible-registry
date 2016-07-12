11f150721f6186f1b34218893a3c010e25f778ce
Jason Callaway <jason@jasoncallaway.com>
2016-07-12 13:10:15 -0400

    Fixed a few stupid things

--- files/docker_sync.py
+++ files/docker_sync.py
@@ -14,20 +14,21 @@ all_images = json.loads(str(urlopen(query_url).read()))
 for image in all_images['results']:
     image_name = image['name']
     if image_name.startswith('openshift') or image_name.startswith('rhel') or image_name.startswith('jboss') or image_name.startswith('rhscl'):
-        print(image_name + ' ', end='')
-        image_tags_url = tags_url.replace('IMAGE', image_name)
-        image_tags = json.loads(str(urlopen(image_tags_url).read()))
-        latest_id = image_tags['latest']
-        all_tags = ['latest']
-        for key in image_tags.keys():
-            if image_tags[key] == latest_id:
-                all_tags.append(key)
-        for tag in all_tags:
-            print(tag + ' ', end='')
-            cli.pull('registry.access.redhat.com/' + image_name, tag)
-            # The push isn't working. For some reason latest_id doesn't jive
-            # with what I'm expecting it to be. For now we'll use the older
-            # docker_load.sh method.
-            # cli.tag(image_name, 'localhost:5000', tag)
-            # cli.push('localhost:5000', image_name, tag, insecure_registry=True)
-        print('\n', end='')
+        if 'beta' not in image_name:
+            print(image_name + ' ', end='')
+            image_tags_url = tags_url.replace('IMAGE', image_name)
+            image_tags = json.loads(str(urlopen(image_tags_url).read()))
+            latest_id = image_tags['latest']
+            all_tags = ['latest']
+            for key in image_tags.keys():
+                if image_tags[key] == latest_id:
+                    all_tags.append(key)
+            for tag in all_tags:
+                print(tag + ' ', end='')
+                cli.pull('registry.access.redhat.com/' + image_name, tag)
+                # The push isn't working. For some reason latest_id doesn't jive
+                # with what I'm expecting it to be. For now we'll use the older
+                # docker_load.sh method.
+                # cli.tag(image_name, 'localhost:5000', tag)
+                # cli.push('localhost:5000', image_name, tag, insecure_registry=True)
+            print('\n', end='')
